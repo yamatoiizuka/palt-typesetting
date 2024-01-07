c42837b8bc6823b69667f4594027f6b833b63013
Yamato <info@yamatoiizuka.com>
2024-01-07 14:33:41 +0900

    Add kerning tag

--- src/utils-tags.ts
+++ src/utils-tags.ts
@@ -1,4 +1,5 @@
 const classnamePrefix = 'typeset'
+const uiIgnore = 'user-select:none;" aria-hidden="true" data-nosnippet=""'
 
 /**
  * `<wbr>`タグはHTML文書内で単語の区切りを示し、必要に応じて改行の挿入を許可するタグです。
@@ -12,7 +13,21 @@ const wbr = '<wbr>'
  */
 const thinSpace = (width: string) => {
   const THIN_SPACE = String.fromCharCode(0x2009) // U+2009 THIN SPACE
-  return `<span style="font-size: ${width}; user-select:none;" aria-hidden="true" data-nosnippet="">${THIN_SPACE}</span>`
+  const classname: string = classnamePrefix + '-thin-space'
+  return `<span class="${classname}" style="font-size: ${width}; ${uiIgnore}>${THIN_SPACE}</span>`
+}
+
+/**
+ * 指定された文字にカーニング（文字間隔調整）を適用します。
+ *
+ * @param char - カーニングを適用する文字。
+ * @param value - カーニング値（千分率）。例: 1000 は 1em のカーニングを意味します。
+ * @return カーニング適用後のHTMLコンテンツ。
+ */
+const applyKerning = (char: string, value: number) => {
+  const emValue = value / 1000 / 2 + 'em'
+  const classname: string = classnamePrefix + '-kerning'
+  return `${char}<span class="${classname}" style="margin: ${emValue}; ${uiIgnore}></span>`
 }
 
 /**
@@ -46,4 +61,4 @@ const applyNoBreakStyle = (segment: string) => {
   return `<span class="${classname}" style="letter-spacing: 0">${segment}</span>`
 }
 
-export { wbr, thinSpace, applyWbrStyle, applyLatinClass, applyNoBreakStyle }
+export { wbr, thinSpace, applyKerning, applyWbrStyle, applyLatinClass, applyNoBreakStyle }
