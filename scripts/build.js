0f7d62052aad001a61bb0268edf09d0aae30a538
Yamato <info@yamatoiizuka.com>
2024-02-08 20:27:33 +0900

    Update build process

---
+++ scripts/build.js
@@ -0,0 +1,28 @@
+const esbuild = require('esbuild')
+const fs = require('fs')
+const path = require('path')
+const { execSync } = require('child_process')
+
+// トランスパイルしたいファイルが含まれるディレクトリ
+const srcDir = './src'
+// 出力先ディレクトリ
+const outDir = './dist'
+
+// TypeScriptコンパイラを使用して型定義ファイルを生成
+execSync('tsc --declaration --emitDeclarationOnly --outDir ' + outDir)
+
+// 指定ディレクトリ内のすべてのファイルを取得
+const files = fs.readdirSync(srcDir).filter(file => file.endsWith('.ts'))
+
+// 各ファイルに対してトランスパイルを実行
+files.forEach(file => {
+  esbuild
+    .build({
+      entryPoints: [path.join(srcDir, file)],
+      outfile: path.join(outDir, file.replace('.ts', '.js')),
+      bundle: false,
+      format: 'esm',
+      platform: 'browser',
+    })
+    .catch(() => process.exit(1))
+})
