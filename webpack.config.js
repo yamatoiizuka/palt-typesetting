734d97270a0e262e2b5f5d56abb347ee908adcc2
Yamato <info@yamatoiizuka.com>
2024-02-20 19:32:41 +0900

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: './dist/index.js',
+  output: {
+    path: path.resolve(__dirname, 'bundle'),
+    filename: 'typesetter.min.js',
+    library: {
+      name: 'Typesetter',
+      type: 'umd',
+      export: 'default',
+    },
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+}
