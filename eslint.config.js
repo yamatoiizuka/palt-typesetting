816d6fe616e6fa039384f83143b32109455fa9c8
Yamato <info@yamatoiizuka.com>
2025-02-16 22:29:44 +0900

    Migrate ESLint configuration from .eslintrc.json to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+  },
+}
